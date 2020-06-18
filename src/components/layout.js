2a5aa79499a8a84a0f5c33d700edd696c05c3ec6
Ekikere-Abasi Michael (Nzaki Michael) <47403757+NzakiCodes@users.noreply.github.com>
2020-06-18 21:21:57 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -122,44 +122,7 @@ export default ({ children, location }) => (
               padding: `0 20px 80px 0`,
             }}
           >
-            powered by&nbsp;
-            <a
-              target="_blank"
-              href="https://gatsbyjs.org"
-              style={{
-                color: '#191919',
-                boxShadow: 'none',
-              }}
-            >
-              <img
-                src={gatsbyLogo}
-                alt="Gatsby JS"
-                style={{
-                  width: '20px',
-                  margin: '0 4px -3px 2px',
-                }}
-              />
-              <strong>Gatsby</strong>
-            </a>
-            &nbsp;and&nbsp;
-            <a
-              target="_blank"
-              href="https://cosmicjs.com"
-              style={{
-                color: '#191919',
-                boxShadow: 'none',
-              }}
-            >
-              <img
-                src={cosmicjsLogo}
-                alt="Cosmic JS"
-                style={{
-                  width: '18px',
-                  margin: '0 4px -2px 5px',
-                }}
-              />
-              <strong>Cosmic JS</strong>
-            </a>
+            &copy; {new Date().getFullYear()} NzakiCodes
           </footer>
         </div>
       )
